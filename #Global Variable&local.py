ce647d82c594d6c02ec8f53835bd4339d7a07d3e
Aravind Reddy Apuri <aravindreddyapuri@Aravinds-MBP.lan>
2024-11-02 23:42:52 -0400

    global & local variables

--- #Global Variable&local.py
+++ #Global Variable&local.py
@@ -2,18 +2,21 @@
 # variables that are created outside of the function are know as global variables
 # global varibales can be used by everyone, both inside or outside of the function
 
-# X = "Aravind awesome" #this is global varibale which is used outside of the function
 
-# def myfunc():
-#     print("python makes " + X)
-
-# myfunc()
-
-#example for local variable
-x = "awesome"
+#example for global variable
+X = "Aravind awesome" #this is global varibale which is used outside of the function
 
 def myfunc():
-    x = "great"
+    print("python makes " + X)
+
+myfunc()
+
+#example for local variable
+
+x = "awesome" 
+
+def myfunc():
+    x = "great" # local variable which is used inside the function
     print("python is " + x)
 
 myfunc()
