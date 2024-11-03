7156aa91a33ff0bed60c64b1043ed0ada5f9b6b9
Aravind Reddy Apuri <aravindreddyapuri@Aravinds-MBP.lan>
2024-11-03 16:58:19 -0500

    all data types

---
+++ # Data_Types.py
@@ -0,0 +1,29 @@
+# Data Types:
+Variables can store data of different types, and different types can do different things.
+Python has the following data type built-in by default, in these categories:
+
+Text type      : str 
+Numeric Types  : int, float, complex
+sequence Types : list, tuple, range
+Mapping Type   : Dict
+Set Types      : Set, frozenset
+Boolean Type   : Bool 
+Binary Types   : bytes, bytearray, memoryview
+none type      : NoneType
+
+x = "Hello World"                                --str
+x = 20                                           --int
+x = 20.5                                         --float
+x = 1j                                           --complex
+x = ["apple", "banana","cherry"]                 --list
+x = ("apple, "banana", "cherry")                 --tuple
+x = range(6)                                     --range
+x = dict (name ="john", age = 36)                --Dict
+x = set(("apple", "banana", "cherry"))           --set
+x = frozenset (("apple", "banana", "cherry"))    --frozenset
+x = bool(5)                                      --Bool
+x = bytes(5)                                     --bytes
+x = byterray(5)                                  --bytearray
+x = memoryview (bytes(5))                        --memoryview
+x = None                                         -- NoneType
+
