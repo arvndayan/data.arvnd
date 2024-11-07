bbf08b2b3a74a7b3105542d5877dd9209aa49703
Aravind Reddy Apuri <aravindreddyapuri@Aravinds-MBP.lan>
2024-11-07 01:05:47 -0500

    slicing concept with examples

---
+++ Slicing.py
@@ -0,0 +1,26 @@
+#Slicing
+# By using slicing you can return range of characters by using slice syntax
+#by specifying the start index and end index, separated by colon to return a part of the string
+
+b = "hello world"
+print(b[2:5])
+#here we are printing the range of characters we need ,this is called slicing
+
+#slice from start
+
+a = "Hai Aravind"
+print(a[:5])
+# this will start from first character
+
+# slice to end
+
+c= "welcome Aravind"
+print(c[2:])
+# it starts from second character and all the way to end
+
+#Negative Indexing
+
+z = "Good Morning"
+print(z[-5:-2])
+
+#slicing starts from the end of the string
